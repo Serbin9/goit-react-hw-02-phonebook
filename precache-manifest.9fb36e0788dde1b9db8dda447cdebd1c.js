7dd3f921620c5f7ef3cd3d50ffe0fc0cc4655177
Serbin9 <serbin_y@ukr.net>
2020-03-11 23:33:18 +0200

    Updates

---
+++ precache-manifest.9fb36e0788dde1b9db8dda447cdebd1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659b623b8dadb08b781e1e2cbdc03e55",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "4b9f2de3e86b821c4edc",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.bdcc3cdf.chunk.css"
+  },
+  {
+    "revision": "139ec9293c8a92b5e684",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.36f09acd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.36f09acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9f2de3e86b821c4edc",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.47f21c32.chunk.js"
+  },
+  {
+    "revision": "f71a2902782a11584a00",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2dad0588.js"
+  }
+]);
